6e9675588466c0c3a836b0eb755d5795c15c1ffe
LeCHAUVEaLaCape <brian.peyre@gmail.com>
2023-02-25 00:11:37 +0100

    read marche

--- client/client.py
+++ client/client.py
@@ -7,20 +7,21 @@ port = 15555
 def commandeHandler(utilisateur,motPasse,stop,read):
     if type(utilisateur) is not Entry or type(motPasse) is not Entry:
         texte_sortie.insert('end', 'Erreur\n') 
-        return
+        pass
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(2)
     try:
         client.connect((hote, port))
-    except (ConnectionAbortedError,ConnectionError,ConnectionRefusedError,ConnectionResetError)as erreur:
+        texte_sortie.delete(1.0, END)
+        if stop:
+            client.send(str.encode(utilisateur.get() + ":" + motPasse.get() + ":s"))
+            texte_sortie.insert('end',client.recv(255))
+        elif read:
+            client.send(str.encode(utilisateur.get() + ":" + motPasse.get() + ":r"))
+            texte_sortie.insert('end',client.recv(255))
+    except (ConnectionAbortedError,ConnectionError,ConnectionRefusedError,ConnectionResetError,TimeoutError,socket.timeout)as erreur:
         texte_sortie.insert('end', 'Erreur: ' + str(erreur) + '\n') 
-        return
-    
-    if stop:
-        client.send(str.encode(utilisateur.get() + ":" + motPasse.get() + ":s"))
-        texte_sortie.insert('end',client.recv(255))
-    elif read:
-        client.send(str.encode(utilisateur.get() + ":" + motPasse.get() + ":r"))
-        texte_sortie.insert('end',client.recv(255))
+        pass
     client.close()
 
 window = Tk()
@@ -39,8 +40,8 @@ motDePasse = Entry(frame_main, show="*")
 motDePasse.pack()
 
 
-btn_stop = Button(frame_main, text="Arrêter",command=lambda: commandeHandler(nomUtilisateur,motDePasse,stop=True)).pack(fill=X, padx=5, pady=5)
-btn_stop = Button(frame_main, text="Lire",command=lambda: commandeHandler(nomUtilisateur,motDePasse,read=True)).pack(fill=X, padx=5, pady=5)
+btn_stop = Button(frame_main, text="Arrêter",command=lambda: commandeHandler(nomUtilisateur,motDePasse,stop=True,read=False)).pack(fill=X, padx=5, pady=5)
+btn_lire = Button(frame_main, text="Lire",command=lambda: commandeHandler(nomUtilisateur,motDePasse,read=True,stop=False)).pack(fill=X, padx=5, pady=5)
 #btn_modif = Button(frame_main, text="Modifier", command=lambda: commandeHandler(nomUtilisateur,motDePasse,)).pack(fill=X, padx=5, pady=5)
 
 # Ajout du champ texte pour afficher les erreurs 
